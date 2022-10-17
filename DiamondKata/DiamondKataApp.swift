c70082ee7b218e61606af33befe3717d4cacefd4
Oleksii Maidanyk <omaid@softserveinc.com>
2022-10-17 09:09:04 +0200

    Create empty app

---
+++ DiamondKata/DiamondKataApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiamondKataApp.swift
+//  DiamondKata
+//
+//  Created by Oleksii Maidanyk on 17.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct DiamondKataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
