0a373340bae847d26512cad14473a8ff677db633
Oleksii Maidanyk <omaid@softserveinc.com>
2022-10-17 10:40:48 +0200

    implement generator logic

--- DiamondKata/DiamondGenerator.swift
+++ DiamondKata/DiamondGenerator.swift
@@ -10,9 +10,27 @@ import Foundation
 final class DiamondGenerator {
   func generate(_ input: String) -> [[String]] {
     guard !input.isEmpty else { return [[]] }
+    let separator = "_"
+    let size = input.count * 2 - 1
+    var result = Array(repeating: Array(repeating: separator, count: size), count: size)
     
-    // generation logic here
-    
-    return [[]]
+    for (index, element) in input.enumerated() {
+      let leftIdx = input.count - 1 - index
+      let rightIdx = input.count - 1 + index
+      let bottomIdx = size - 1 - index
+      
+      result[index][leftIdx] = String(element)
+      if leftIdx != rightIdx {
+        result[index][rightIdx] = String(element)
+      }
+      
+      if index != bottomIdx {
+        result[bottomIdx][leftIdx] = String(element)
+        if leftIdx != rightIdx {
+          result[bottomIdx][rightIdx] = String(element)
+        }
+      }
+    }
+    return result
   }
 }
--- DiamondKataTests/DiamondKataTests.swift
+++ DiamondKataTests/DiamondKataTests.swift
@@ -90,11 +90,4 @@ final class DiamondKataTests: XCTestCase {
     }
   }
   
-  func testPerformanceExample() throws {
-    // This is an example of a performance test case.
-    self.measure {
-      // Put the code you want to measure the time of here.
-    }
-  }
-  
 }
